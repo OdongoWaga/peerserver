75a0dbbb51bf165b7e446a7a0c6db3b7527e6603
Waga Odongo <wagaodongo@gmail.com>
2019-02-04 10:32:41 +0300

    add
    Signed-off-by: Waga Odongo <wagaodongo@gmail.com>

---
+++ app.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var path = require('path');
+var http = require('http');
+var cors = require('cors');
+var errorhandler = require('errorhandler');
+var ExpressPeerServer =require('peer').ExpressPeerServer;
+
+var options = {
+    debug:true,
+    key: 'codeit'
+};
+
+var app = express();
+
+var server = http.createServer(app);
+
+var port = process.env.PORT || '3000';
+
+app.ser('port', port);
+
+app.use(cors());
+app.use(express.static(path.join(_dirname,'public')));
+app.use('/peerjs', ExpressPeerServer(server, options));
+
+app.use(errorhandler());
+
+process.on('uncaughtException', function(exc){
+    console.error(exc);
+});
+
+server.listen(port);
